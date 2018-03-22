72199d834447457240b4714296a1b243611dbfc6
daniel1147 <sxn91401@gmail.com>
2018-03-22 11:02:53 +0000

    rename hello_world, two_sum

---
+++ hello_world/HelloWorld.java
@@ -0,0 +1,7 @@
+package codility.hello_world;
+
+public class HelloWorld {
+  public void run() {
+    System.out.println("Hello, world!");
+  }
+}
---
+++ hello_world/TestHelloWorld.java
@@ -0,0 +1,9 @@
+package codility.hello_world;
+
+class TestHelloWorld {
+  public static void main(String[] args) {
+    HelloWorld helloWorld = new HelloWorld();
+
+    helloWorld.run();
+  }
+}
