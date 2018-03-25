32b8fe7ec94ef6f945e85a0db849a7370f6aaf51
daniel1147 <sxn91401@gmail.com>
2018-03-25 14:23:36 +0000

    [Solution] count factors 100% by using Math.sqrt

--- count_factors/Solution.java
+++ count_factors/Solution.java
@@ -4,14 +4,20 @@ class Solution {
   public int solution(int N) {
     int counter = 0;
     int i;
-    for (i = 1; i * i < N; i++) {
+
+    int sqrtN = (int) Math.sqrt(N);
+
+    for (i = 1; i <= sqrtN; i++) {
       if (N % i == 0) {
         counter += 2;
       }
     }
 
+    System.out.printf("i = %d, sqrt of n is %d\n", i, sqrtN);
+
+    i = i - 1;
     if (i * i == N) {
-      counter++;
+      counter -= 1;
     }
 
     return counter;
