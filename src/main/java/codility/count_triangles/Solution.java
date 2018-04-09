e1c3c490b0500b0e782031433ad3c38a21b998cb
daniel1147 <sxn91401@gmail.com>
2018-04-09 07:57:01 +0000

    [Solution] count triangles 100%. O(n^2)

--- src/main/java/codility/count_triangles/Solution.java
+++ src/main/java/codility/count_triangles/Solution.java
@@ -8,6 +8,7 @@ class Solution {
   private int counter;
   private int currentCounter;
   private int currentSum;
+  private int line3;
 
   public int solution(int[] A) {
     N = A.length;
@@ -21,14 +22,21 @@ class Solution {
     counter = 0;
 
     for (line1 = 0; line1 < N - 2; line1++) {
-      for (line2 = line1 + 1; line2 < N - 1; line2++) {
-        currentCounter = 0;
+      currentCounter = 0;
+      line2 = line1 + 1;
+      line3 = line2 + 1;
+      for (; line2 < N - 1; line2++) {
         currentSum = A[line1] + A[line2];
-        for (int i = line2 + 1; i < N; i++) {
-          if (A[i] >= currentSum) {
-            break;
-          }
+        if (line3 <= line2) {
+          line3 = line2 + 1;
+          currentCounter = 0;
+        }
 
+        if (currentCounter > 0) {
+          currentCounter--;
+        }
+
+        for (; line3 < N && A[line3] < currentSum; line3++) {
           currentCounter++;
         }
 
