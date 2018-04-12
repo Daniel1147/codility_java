0b931f9445b477c117d0890487d08d06e68f1043
daniel1147 <sxn91401@gmail.com>
2018-04-12 08:54:05 +0000

    [Solution] flood depth 100%

--- src/test/java/codility/flood_depth/TestSolution.java
+++ src/test/java/codility/flood_depth/TestSolution.java
@@ -24,6 +24,70 @@ public class TestSolution {
     assertTrue(getFailMsg(), true);
   }
 
+  @Test
+  public void TestSingle() {
+    A = new int[] {47};
+    expected = 0;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestTwo1() {
+    A = new int[] {47, 48};
+    expected = 0;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestTwo2() {
+    A = new int[] {49, 48};
+    expected = 0;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestThree1() {
+    A = new int[] {49, 48, 47};
+    expected = 0;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestThree2() {
+    A = new int[] {47, 48, 49};
+    expected = 0;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestThree3() {
+    A = new int[] {49, 48, 49};
+    expected = 1;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestThree4() {
+    A = new int[] {47, 48, 47};
+    expected = 0;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void Test1() {
+    A = new int[] {1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2};
+    expected = 2;
+    actual = s.solution(A);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
   private String getFailMsg() {
     return String.format("input: A %s\n", Arrays.toString(A));
   }
