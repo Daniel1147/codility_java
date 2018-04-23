236b6d1163ba58622d7dee4c650a1d9d163daff1
daniel1147 <sxn91401@gmail.com>
2018-04-23 06:58:25 +0000

    buildPackage.sh remove unusing variable, export TEST_CLASS

--- buildPackage.sh
+++ buildPackage.sh
@@ -8,12 +8,9 @@ if [ -z ${NEW_PACKAGE+x} ]; then
     read -p $'new package name(NEW_PACKAGE): \n' NEW_PACKAGE
 fi
 
-TEMPLATE_PACKAGE=min_max_division
-TEMPLATE_SOLUTION=Solution.java
-TEMPLATE_TEST=TestMinMaxDivision.java
-
 mkdir "$MAIN_ROOT/$NEW_PACKAGE"
 mkdir "$TEST_ROOT/$NEW_PACKAGE"
 
 cp $SOLUTION_TEMPLATE "$MAIN_ROOT/$NEW_PACKAGE/Solution.java"
 cp $TEST_TEMPLATE "$TEST_ROOT/$NEW_PACKAGE/TestSolution.java"
+export TEST_CLASS="$ROOT_PACKAGE.$NEW_PACKAGE.TestSolution"
