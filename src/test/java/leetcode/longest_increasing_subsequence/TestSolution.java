eef9589b6bfd7f128ba6b39e9d7abf4478be85f4
Daniel1147 <sxn91401@gmail.com>
2018-05-02 15:52:30 +0800

    test Solution2, add tests

--- src/test/java/leetcode/longest_increasing_subsequence/TestSolution.java
+++ src/test/java/leetcode/longest_increasing_subsequence/TestSolution.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 public class TestSolution {
   private int expected, actual;
   private int[] input;
-  private Solution s;
+  private Solution2 s;
 
   @Before
   public void setUp() {
-    s = new Solution();
+    s = new Solution2();
   }
 
   @Test
@@ -21,6 +21,42 @@ public class TestSolution {
     input = new int[0];
     actual = s.lengthOfLIS(input);
     assertTrue(getFailMsg(), true);
+  }
+
+  @Test
+  public void TestEmpty() {
+    input = new int[] {};
+    expected = 0;
+    actual = s.lengthOfLIS(input);
+
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestSingle() {
+    input = new int[] {1};
+    expected = 1;
+    actual = s.lengthOfLIS(input);
+
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestAllIncrease() {
+    input = new int[] {1, 2, 3, 4, 5, 6};
+    expected = 6;
+    actual = s.lengthOfLIS(input);
+
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  @Test
+  public void TestAllDecrease() {
+    input = new int[] {6, 5, 4, 3, 2, 1};
+    expected = 1;
+    actual = s.lengthOfLIS(input);
+
+    assertEquals(getFailMsg(), expected, actual);
   }
 
   @Test
