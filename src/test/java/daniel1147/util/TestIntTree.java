95d6c0b4ba7311361655b2638c721313882c0172
Daniel1147 <sxn91401@gmail.com>
2018-05-04 14:11:15 +0800

    add TestInOrder()

--- src/test/java/daniel1147/util/TestIntTree.java
+++ src/test/java/daniel1147/util/TestIntTree.java
@@ -7,13 +7,7 @@ import static org.junit.Assert.assertArrayEquals;
 public class TestIntTree {
   @Test
   public void TestPostOrder() {
-    IntTree root = new IntTree(1);
-    root.l = new IntTree(2);
-    root.l.l = new IntTree(4);
-    root.l.r = new IntTree(5);
-    root.r = new IntTree(3);
-    root.r.l = new IntTree(6);
-
+    IntTree root = sample1();
     int[] expected, postOrder;
 
     expected = new int[] {4, 5, 2, 6, 3, 1};
@@ -21,5 +15,28 @@ public class TestIntTree {
 
     String errorMsg = String.format("expected => %s, actual => %s\n", Arrays.toString(expected), Arrays.toString(postOrder));
     assertArrayEquals(errorMsg, expected, postOrder);
+  }
+
+  @Test
+  public void TestInOrder() {
+    IntTree root = sample1();
+    int[] expected, inOrder;
+
+    expected = new int[] {4, 2, 5, 1, 6, 3};
+    inOrder = root.inOrder();
+
+    String errorMsg = String.format("expected => %s, actual => %s\n", Arrays.toString(expected), Arrays.toString(inOrder));
+    assertArrayEquals(errorMsg, expected, inOrder);
+  }
+
+  private IntTree sample1() {
+    IntTree root = new IntTree(1);
+    root.l = new IntTree(2);
+    root.l.l = new IntTree(4);
+    root.l.r = new IntTree(5);
+    root.r = new IntTree(3);
+    root.r.l = new IntTree(6);
+
+    return root;
   }
 }
