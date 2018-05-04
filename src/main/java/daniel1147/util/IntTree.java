3998e979e819f8a81bc3fe368585366e428a8f81
Daniel1147 <sxn91401@gmail.com>
2018-05-04 14:24:43 +0800

    add inOrder(), extract to method concat

--- src/main/java/daniel1147/util/IntTree.java
+++ src/main/java/daniel1147/util/IntTree.java
@@ -11,24 +11,36 @@ public class IntTree {
     this.x = x;
   }
 
+  public int[] inOrder() {
+    int[] result, left, right;
+
+    left = (l != null) ? l.inOrder() : new int[0];
+    right = (r != null) ? r.inOrder() : new int[0];
+
+    result = concat(left, new int[]{x}, right);
+
+    return result;
+  }
+
   public int[] postOrder() {
     int[] result, left, right;
 
-    if (l == null)
-      left = new int[0];
-    else
-      left = l.postOrder();
+    left = (l != null) ? l.postOrder() : new int[0];
+    right = (r != null) ? r.postOrder() : new int[0];
 
-    if (r == null)
-      right = new int[0];
-    else
-      right = r.postOrder();
+    result = concat(left, right, new int[] {x});
 
-    result = new int[left.length + right.length + 1];
+    return result;
+  }
 
-    System.arraycopy(left, 0, result, 0, left.length);
-    System.arraycopy(right, 0, result, left.length, right.length);
-    result[left.length + right.length] = x;
+  private int[] concat(int[] a, int[] b, int[] c) {
+    int[] result;
+
+    result = new int[a.length + b.length + c.length];
+
+    System.arraycopy(a, 0, result, 0, a.length);
+    System.arraycopy(b, 0, result, a.length, b.length);
+    System.arraycopy(c, 0, result, a.length + b.length, c.length);
 
     return result;
   }
