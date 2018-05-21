e865fb424a7d706931a37a2503722429caba7b7e
Daniel1147 <sxn91401@gmail.com>
2018-05-21 14:50:12 +0800

    [Solution] solved by changing visited node. beats 98.8%

--- src/main/java/leetcode/linked_list_cycle/Solution.java
+++ src/main/java/leetcode/linked_list_cycle/Solution.java
@@ -4,6 +4,19 @@ import java.util.*;
 
 class Solution {
   public boolean hasCycle(ListNode head) {
+    ListNode falseNode, currentNode, nextNode;
+
+    falseNode = new ListNode(0);
+    currentNode = head;
+    while(currentNode != null) {
+      if (currentNode.next == falseNode)
+        return true;
+
+      nextNode = currentNode.next;
+      currentNode.next = falseNode;
+      currentNode = nextNode;
+    }
+
     return false;
   }
 }
