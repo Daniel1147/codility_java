bcf93de85a0a6d469feba1c5a58650002722499d
Daniel1147 <sxn91401@gmail.com>
2018-05-21 13:34:13 +0800

    add frame code

---
+++ src/main/java/leetcode/single_number/Solution.java
@@ -0,0 +1,9 @@
+package leetcode.single_number;
+
+import java.util.*;
+
+class Solution {
+  public int solution() {
+    return 0;
+  }
+}
---
+++ src/test/java/leetcode/single_number/TestSolution.java
@@ -0,0 +1,31 @@
+package leetcode.single_number;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSolution {
+  private int expected, actual;
+  private Solution s;
+
+  @Before
+  public void setUp() {
+    s = new Solution();
+  }
+
+  @Test
+  public void testCompile() {
+    assertTrue(true);
+  }
+
+  private void myTest() {
+    assertEquals(getFailMsg(), 0, 0);
+  }
+
+  private String getFailMsg() {
+    return "";
+  }
+}
