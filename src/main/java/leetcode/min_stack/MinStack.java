77c942e498ecb0048c15d1b150ebe1d0d63088df
Daniel1147 <sxn91401@gmail.com>
2018-05-21 15:26:43 +0800

    implement by java.util.Stack

--- src/main/java/leetcode/min_stack/MinStack.java
+++ src/main/java/leetcode/min_stack/MinStack.java
@@ -1,25 +1,58 @@
 package leetcode.min_stack;
 
-import java.util.*;
+import java.util.Stack;
 
 class MinStack {
-  public MinStack() {}
+  private static final int INVALID = -1;
+
+  private Stack<Integer> s;
+  private int min;
+
+  public MinStack() {
+    s = new Stack<Integer>();
+  }
 
   public void push(int x) {
-    // TODO
+    if (s.empty())
+      min = x;
+    else
+      updateMin(x);
+
+    s.push(x);
   }
 
   public void pop() {
-    // TODO
+    int n;
+
+    n = s.pop();
+    if (n == min)
+      updateMin();
   }
 
   public int top() {
-    // TODO
-    return -1;
+    return s.peek();
   }
 
   public int getMin() {
-    // TODO
-    return -1;
+    if (s.empty())
+      return INVALID;
+
+    return min;
+  }
+
+  private void updateMin(int x) {
+    if (min > x)
+      min = x;
+  }
+
+  private void updateMin() {
+    if (s.empty())
+      min = INVALID;
+
+    min = s.peek();
+    for (Integer num : s) {
+      if (min > num)
+        min = num;
+    }
   }
 }
