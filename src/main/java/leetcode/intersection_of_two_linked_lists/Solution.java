cf71a1f631abd524ec7974367c6e5ad35c624531
Daniel1147 <sxn91401@gmail.com>
2018-05-22 14:07:08 +0800

    FormatCode

--- src/main/java/leetcode/intersection_of_two_linked_lists/Solution.java
+++ src/main/java/leetcode/intersection_of_two_linked_lists/Solution.java
@@ -7,25 +7,19 @@ class Solution {
   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
     ListNode A, B;
 
-    if (headA == null || headB == null)
-      return null;
+    if (headA == null || headB == null) return null;
 
     A = headA;
     B = headB;
 
     while (A != B) {
-      if (A.next == null && B.next == null)
-        return null;
+      if (A.next == null && B.next == null) return null;
 
-      if (A.next == null)
-        A = headB;
-      else
-        A = A.next;
+      if (A.next == null) A = headB;
+      else A = A.next;
 
-      if (B.next == null)
-        B = headA;
-      else
-        B = B.next;
+      if (B.next == null) B = headA;
+      else B = B.next;
     }
 
     return A;
