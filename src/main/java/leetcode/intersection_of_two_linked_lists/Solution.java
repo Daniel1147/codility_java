006fe1ff7c90f1aa280ff22c2dbf48a001c88341
Daniel1147 <sxn91401@gmail.com>
2018-05-22 13:53:20 +0800

    add frame code and a test

---
+++ src/main/java/leetcode/intersection_of_two_linked_lists/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.intersection_of_two_linked_lists;
+
+import java.util.*;
+import leetcode.util.ListNode;
+
+class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    return headA;
+  }
+}
---
+++ src/test/java/leetcode/intersection_of_two_linked_lists/TestSolution.java
@@ -0,0 +1,46 @@
+package leetcode.intersection_of_two_linked_lists;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import leetcode.util.ListNode;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSolution {
+  private ListNode expected, actual, headA, headB;
+  private Solution s;
+
+  @Before
+  public void setUp() {
+    s = new Solution();
+  }
+
+  @Test
+  public void testCompile() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void testLeetcode() {
+    ListNode.Factory f = new ListNode.Factory();
+
+    headA = f.fromNumArray(new int[] {1, 2, 3, 4, 5});
+
+    headB = f.fromNumArray(new int[] {6, 7, 8});
+    headB.next.next.next = headA.next.next;
+
+    expected = headA.next.next;
+    myTest();
+  }
+
+  private void myTest() {
+    actual = s.getIntersectionNode(headA, headB);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  private String getFailMsg() {
+    return String.format("headA => %s\n headB => %s\n", headA, headB);
+  }
+}
