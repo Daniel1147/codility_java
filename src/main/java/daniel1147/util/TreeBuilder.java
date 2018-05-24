aebaa218f75406e91570f0017d47831a26f20aec
Daniel1147 <sxn91401@gmail.com>
2018-05-25 03:39:33 +0800

    add daniel1147.util.TreeBuilder

---
+++ src/main/java/daniel1147/util/TreeBuilder.java
@@ -0,0 +1,67 @@
+package daniel1147.util;
+
+import java.util.*;
+
+public class TreeBuilder {
+  private static final String nullNode = "x";
+  private ITreeOperator o;
+
+  public TreeBuilder(ITreeOperator o) {
+    this.o = o;
+  }
+
+  public void levelOrderConstruct(String s) {
+    if (s.equals("")) {
+      o.setRootNull();
+
+      return;
+    }
+
+    // handle string
+    String[] nodeStrList;
+    Queue<String> q;
+
+    nodeStrList = s.split(" ");
+    q = new ArrayDeque<String>(Arrays.asList(nodeStrList));
+
+    // init root
+    String nodeStr;
+    ITree root, node, parentNode;
+    Queue<ITree> nodeQ;
+
+    nodeQ = new ArrayDeque<ITree>();
+    nodeStr = q.poll();
+    if (nodeStr.equals(nullNode))
+      return;
+
+    root = o.initRoot(parseStr(nodeStr));
+    nodeQ.add(root);
+
+    // process child
+    while(!q.isEmpty()) {
+      nodeStr = q.poll();
+      parentNode = nodeQ.poll();
+      if (!nodeStr.equals(nullNode)) {
+        node = o.lAppend(parentNode, parseStr(nodeStr));
+        nodeQ.add(node);
+      } else {
+        o.lAppendNull(parentNode);
+      }
+
+      if (q.isEmpty())
+        break;
+
+      nodeStr = q.poll();
+      if (!nodeStr.equals(nullNode)) {
+        node = o.rAppend(parentNode, parseStr(nodeStr));
+        nodeQ.add(node);
+      } else {
+        o.rAppendNull(parentNode);
+      }
+    }
+  }
+
+  private int parseStr(String s) {
+    return Integer.parseInt(s);
+  }
+}
