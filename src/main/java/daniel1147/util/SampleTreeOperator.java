07e1995ba24c31756924b26bfdb6beaaf700b8c9
Daniel1147 <sxn91401@gmail.com>
2018-05-25 03:57:51 +0800

    avoid using down casting in tree operator

--- src/main/java/daniel1147/util/SampleTreeOperator.java
+++ src/main/java/daniel1147/util/SampleTreeOperator.java
@@ -17,35 +17,7 @@ class SampleTreeOperator implements ITreeOperator {
     return root;
   }
 
-  public ITree lAppend(ITree parent, int value) {
-    SampleTree n = (SampleTree) parent;
-    n.lt = create(value);
-
-    return n.lt;
-  }
-
-  public ITree rAppend(ITree parent, int value) {
-    SampleTree n = (SampleTree) parent;
-    n.rt = create(value);
-
-    return n.rt;
-  }
-
-  public void lAppendNull(ITree parent) {
-    SampleTree n = (SampleTree) parent;
-    n.lt = null;
-  }
-
-  public void rAppendNull(ITree parent) {
-    SampleTree n = (SampleTree) parent;
-    n.rt = null;
-  }
-
   public void setRootNull() {
     root = null;
-  }
-
-  private SampleTree create(int value) {
-    return new SampleTree(value);
   }
 }
