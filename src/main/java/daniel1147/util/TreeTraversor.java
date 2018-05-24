926b4e923450939c0e6bc1dd1d4ea97a324d7243
Daniel1147 <sxn91401@gmail.com>
2018-05-25 04:33:50 +0800

    daniel1147.util.TreeTraversor add methods inOrder(), postOrder(), levelOrder()

--- src/main/java/daniel1147/util/TreeTraversor.java
+++ src/main/java/daniel1147/util/TreeTraversor.java
@@ -1,15 +1,54 @@
 package daniel1147.util;
 
-class TreeTraversor
-{
-  private ITree tree;
+import java.util.*;
 
-  public TreeTraversor(ITree tree) {
-    this.tree = tree;
+public class TreeTraversor
+{
+  private ITree root;
+
+  public TreeTraversor(ITree root) {
+    this.root = root;
   }
 
   public int[] preOrder() {
-    return preOrderTree(tree);
+    return preOrderTree(root);
+  }
+
+  public int[] inOrder() {
+    return inOrderTree(root);
+  }
+
+  public int[] postOrder() {
+    return postOrderTree(root);
+  }
+
+  public int[] levelOrder() {
+    Queue<ITree> q;
+    List<Integer> result;
+
+    q = new ArrayDeque<ITree>();
+    result = new ArrayList<Integer>();
+
+    q.add(root);
+
+    ITree t;
+    while (!q.isEmpty()) {
+      t = q.poll();
+
+      if (t.left() != null) q.add(t.left());
+
+      if (t.right() != null) q.add(t.right());
+
+      result.add(t.value());
+    }
+
+    int[] output;
+    output = new int[result.size()];
+    Iterator<Integer> iter = result.iterator();
+
+    for (int i = 0; iter.hasNext(); i++) output[i] = iter.next();
+
+    return output;
   }
 
   private int[] preOrderTree(ITree tree) {
@@ -25,6 +64,40 @@ class TreeTraversor
 
     currentValue = tree.value();
     result = concatIntArr(new int[] {currentValue}, lResult, rResult);
+
+    return result;
+  }
+
+  private int[] inOrderTree(ITree tree) {
+    int[] result, lResult, rResult;
+    ITree lTree, rTree;
+    int currentValue;
+
+    lTree = tree.left();
+    rTree = tree.right();
+
+    lResult = (lTree != null) ? inOrderTree(lTree) : new int[0];
+    rResult = (rTree != null) ? inOrderTree(rTree) : new int[0];
+
+    currentValue = tree.value();
+    result = concatIntArr(lResult, new int[] {currentValue}, rResult);
+
+    return result;
+  }
+
+  private int[] postOrderTree(ITree tree) {
+    int[] result, lResult, rResult;
+    ITree lTree, rTree;
+    int currentValue;
+
+    lTree = tree.left();
+    rTree = tree.right();
+
+    lResult = (lTree != null) ? postOrderTree(lTree) : new int[0];
+    rResult = (rTree != null) ? postOrderTree(rTree) : new int[0];
+
+    currentValue = tree.value();
+    result = concatIntArr(lResult, rResult, new int[] {currentValue});
 
     return result;
   }
