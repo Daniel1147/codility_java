b69748ac20c466a6c4a0d23b5fead5b748341d63
Daniel1147 <sxn91401@gmail.com>
2018-05-25 19:00:22 +0800

    add solution frame

---
+++ src/main/java/leetcode/palindrome_linked_list/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.palindrome_linked_list;
+
+import leetcode.util.ListNode;
+
+class Solution {
+  public boolean isPalindrome(ListNode head) {
+    // TODO
+    return false;
+  }
+}
