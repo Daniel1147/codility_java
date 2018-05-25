a49a38ddede818d4f67e79f2ce529dbc4cd5a43e
Daniel1147 <sxn91401@gmail.com>
2018-05-25 13:50:32 +0800

    add TestListBuilder

---
+++ src/test/java/daniel1147/util/TestListBuilder.java
@@ -0,0 +1,47 @@
+package daniel1147.util;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class TestListBuilder {
+  ListConvertor lc;
+  ListBuilder lb;
+  IListNode head;
+  int[] expect, actual;
+
+  @Before
+  public void setUp() {
+    lc = new ListConvertor();
+    head = new SampleListNode(0);
+    lb = new ListBuilder(head);
+  }
+
+  @Test
+  public void test1() {
+    expect = new int[0];
+    head = lb.fromNumArray(expect);
+    actual = lc.toIntArray(head);
+
+    assertArrayEquals(expect, actual);
+  }
+
+  @Test
+  public void test2() {
+    expect = new int[] {10};
+    head = lb.fromNumArray(expect);
+    actual = lc.toIntArray(head);
+
+    assertArrayEquals(expect, actual);
+  }
+
+  @Test
+  public void test3() {
+    expect = new int[] {1, 2, 3, 4, 5, 6};
+    head = lb.fromNumArray(expect);
+    actual = lc.toIntArray(head);
+
+    assertArrayEquals(expect, actual);
+  }
+}
