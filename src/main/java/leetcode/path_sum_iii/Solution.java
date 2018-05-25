7a2292dd5a5c1a0ceb57bb976f3ec78a95bfe17d
Daniel1147 <sxn91401@gmail.com>
2018-05-26 02:55:07 +0800

    pass test

--- src/main/java/leetcode/path_sum_iii/Solution.java
+++ src/main/java/leetcode/path_sum_iii/Solution.java
@@ -1,11 +1,49 @@
 package leetcode.path_sum_iii;
 
 import leetcode.util.TreeNode;
+import java.util.Stack;
 
 class Solution {
-  public int pathSum(TreeNode root, int sum) {
-    // TODO
+  int sum;
+  Stack<Integer> s;
 
-    return 0;
+  public int pathSum(TreeNode root, int sum) {
+    this.sum = sum;
+    s = new Stack<Integer>();
+
+    return helper(root);
+  }
+
+  private int helper(TreeNode node) {
+    int result, currentSum, parentSum;
+
+    if (s.empty())
+      parentSum = 0;
+    else
+      parentSum = s.peek();
+
+    currentSum = node.val + parentSum;
+
+    if (node.val == sum)
+      result = 1;
+    else
+      result = 0;
+
+    for (Integer ancestorSum : s) {
+      if (currentSum - ancestorSum == sum)
+        result++;
+    }
+
+    s.push(currentSum);
+
+    if (node.left != null)
+      result += helper(node.left);
+
+    if (node.right != null)
+      result += helper(node.right);
+
+    s.pop();
+
+    return result;
   }
 }
