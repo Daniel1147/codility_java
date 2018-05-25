c98ee6ecc4f9d16879c0a19586de1e6b0f0f0ead
Daniel1147 <sxn91401@gmail.com>
2018-05-25 12:13:30 +0800

    use daniel1147.util.* in codility.tree_height.TestSolution

--- src/test/java/codility/tree_height/TestSolution.java
+++ src/test/java/codility/tree_height/TestSolution.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import codility.util.Tree;
+import codility.util.TreeOperator;
+import daniel1147.util.TreeBuilder;
+import daniel1147.util.TreeTraversor;
 
 public class TestSolution {
   private Tree t;
@@ -25,7 +28,7 @@ public class TestSolution {
 
   @Test
   public void TestEmpty() {
-    t = Tree.levelOrderConstruct("");
+    t = levelOrderConstruct("");
     expected = -1;
 
     myTest();
@@ -33,7 +36,7 @@ public class TestSolution {
 
   @Test
   public void Test1() {
-    t = Tree.levelOrderConstruct("1 2 3 4 x x x 5");
+    t = levelOrderConstruct("1 2 3 4 x x x 5");
     expected = 3;
 
     myTest();
@@ -41,7 +44,7 @@ public class TestSolution {
 
   @Test
   public void TestCodility1() {
-    t = Tree.levelOrderConstruct("5 3 10 20 21 1");
+    t = levelOrderConstruct("5 3 10 20 21 1");
     expected = 2;
 
     myTest();
@@ -54,12 +57,22 @@ public class TestSolution {
 
   private String getFailMsg() {
     String inputStr;
+    TreeTraversor tt = new TreeTraversor(t);
 
     if (t == null)
       inputStr = "[]";
     else
-      inputStr = Arrays.toString(t.inOrder());
+      inputStr = Arrays.toString(tt.inOrder());
 
     return String.format("input: %s\n", inputStr);
+  }
+
+  private Tree levelOrderConstruct(String s) {
+    TreeOperator to = new TreeOperator();
+    TreeBuilder tb = new TreeBuilder(to);
+
+    tb.levelOrderConstruct(s);
+
+    return to.getTree();
   }
 }
