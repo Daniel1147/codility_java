606d9e69e6bb6d97f41e477e466f8449ddedcdcf
Daniel1147 <sxn91401@gmail.com>
2018-05-26 02:00:40 +0800

    [Solution] beats 100%

--- src/main/java/leetcode/move_zeroes/Solution.java
+++ src/main/java/leetcode/move_zeroes/Solution.java
@@ -2,7 +2,25 @@ package leetcode.move_zeroes;
 
 class Solution {
   public void moveZeroes(int[] nums) {
-    // TODO
+    int slow, fast;
+
+    fast = 0;
+    slow = 0;
+
+    while (fast < nums.length) {
+      if (nums[fast] != 0) {
+        nums[slow] = nums[fast];
+        slow++;
+      }
+
+      fast++;
+    }
+
+    while (slow < nums.length) {
+      nums[slow] = 0;
+      slow++;
+    }
+
     return;
   }
 }
