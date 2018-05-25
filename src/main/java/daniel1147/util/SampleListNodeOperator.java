e5ae6235659a4b9489c262ec1d552c210ee218db
Daniel1147 <sxn91401@gmail.com>
2018-05-25 14:19:26 +0800

    add SampleListNodeOperator

---
+++ src/main/java/daniel1147/util/SampleListNodeOperator.java
@@ -0,0 +1,37 @@
+package daniel1147.util;
+
+public class SampleListNodeOperator implements IListNodeOperator {
+  SampleListNode root, tail;
+
+  public SampleListNodeOperator() {
+    root = null;
+    tail = null;
+  }
+
+  public IListNode getListNodeRoot() {
+    return root;
+  }
+
+  public SampleListNode getSampleListNodeRoot() {
+    return root;
+  }
+
+  public void add(int value) {
+    if (root == null) {
+      root = create(value);
+      tail = root;
+
+      return;
+    }
+
+    SampleListNode newNode;
+
+    newNode = create(value);
+    tail.next = newNode;
+    tail = newNode;
+  }
+
+  public SampleListNode create(int value) {
+    return new SampleListNode(value);
+  }
+}
