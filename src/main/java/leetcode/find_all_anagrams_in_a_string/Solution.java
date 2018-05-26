d3140851617c688b54177d5dc73f3e966f6626cf
Daniel1147 <sxn91401@gmail.com>
2018-05-26 23:25:55 +0800

    [Solution] beats 51.42%

--- src/main/java/leetcode/find_all_anagrams_in_a_string/Solution.java
+++ src/main/java/leetcode/find_all_anagrams_in_a_string/Solution.java
@@ -4,8 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-  public List<Integer> findAnagrams(String s, String p) {
+  int[] charLeft;
 
-    return null;
+  public List<Integer> findAnagrams(String s, String p) {
+    List<Integer> result;
+
+    buildCharLeft(p);
+    result = process(s, p.length());
+
+    return result;
+  }
+
+  private void buildCharLeft(String p) {
+    char c;
+    int indexC;
+
+    charLeft = new int[26];
+
+    for (int i = 0; i < p.length(); i++) {
+      indexC = cIndex(p.charAt(i));
+      charLeft[indexC]++;
+    }
+
+    return;
+  }
+
+  private List<Integer> process(String s, int pLen) {
+    int slow, fast, matchCount, indexNewC, oldIndexC;
+    List<Integer> result;
+
+    slow = 0;
+    fast = 0;
+    matchCount = 0;
+    result = new ArrayList<Integer>();
+
+    while (fast < s.length()) {
+      // System.out.println(String.format("slow => %d, fast => %d, matchCount => %d", slow, fast, matchCount));
+      indexNewC = cIndex(s.charAt(fast));
+      charLeft[indexNewC]--;
+      if (charLeft[indexNewC] >= 0)
+        matchCount++;
+      fast++;
+
+      if (matchCount == pLen) {
+        result.add(slow);
+      }
+
+      if (fast - slow == pLen) {
+        oldIndexC = cIndex(s.charAt(slow));
+        charLeft[oldIndexC]++;
+
+        if (charLeft[oldIndexC] > 0)
+          matchCount--;
+
+        slow++;
+      }
+    }
+
+    return result;
+  }
+
+  private int cIndex(char c) {
+    return c - 'a';
   }
 }
