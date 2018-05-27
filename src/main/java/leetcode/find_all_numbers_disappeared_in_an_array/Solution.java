2c0b8e70cf2b687b63d18a02d12ca65f94b530ad
Daniel1147 <sxn91401@gmail.com>
2018-05-27 12:56:34 +0800

    [Solution] instead of marking exist num with negative, add num.length to them. beats 96.96%

--- src/main/java/leetcode/find_all_numbers_disappeared_in_an_array/Solution.java
+++ src/main/java/leetcode/find_all_numbers_disappeared_in_an_array/Solution.java
@@ -1,34 +1,24 @@
 package leetcode.find_all_numbers_disappeared_in_an_array;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
   public List<Integer> findDisappearedNumbers(int[] nums) {
-    int value, targetValue;
     List<Integer> result = new ArrayList<Integer>();
+    int n;
 
-    if (nums == null || nums.length < 2)
-      return result;
+    n = nums.length;
 
-    for (int i = 0; i < nums.length; i++) {
-      value = nums[i];
-      if (value < 0)
-        value = value * -1;
+    for (int i = 0; i < n; i++)
+      nums[(nums[i] - 1) % n] += n;
 
-      targetValue = nums[value - 1];
-      if (targetValue > 0)
-        nums[value - 1]  = targetValue * -1;
-    }
-
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] > 0) {
+    for (int i = 0; i < n; i++) {
+      if (nums[i] <= n)
         result.add(i + 1);
-      }
     }
+
 
     return result;
-
   }
 }
