61050f7e4dcce269378df61e6fd460e667f41b23
Daniel1147 <sxn91401@gmail.com>
2018-05-27 16:36:42 +0800

    [Solution] use hashmap and implement dp. beats 88.50%

--- src/main/java/leetcode/subarray_sum_equals_k/Solution.java
+++ src/main/java/leetcode/subarray_sum_equals_k/Solution.java
@@ -1,27 +1,30 @@
 package leetcode.subarray_sum_equals_k;
 
+import java.util.HashMap;
+
 class Solution {
   public int subarraySum(int[] nums, int k) {
-    int[] leftSum;
-    int result, sumToI;
+    int result, sum, oldHash, remainCount;
+    HashMap<Integer, Integer> m;
 
-    if (nums == null || nums.length == 0)
-      return 0;
-
-    leftSum = new int[nums.length + 1];
-
-    for (int i = 0; i < nums.length; i++) {
-      leftSum[i + 1] = leftSum[i] + nums[i];
-    }
-
+    m = new HashMap<>();
     result = 0;
+    sum = 0;
+
+    m.put(0, 1);
     for (int i = 0; i < nums.length; i++) {
-      sumToI = leftSum[i + 1];
-      for (int j = 0; j <= i; j++) {
-        if (sumToI - leftSum[j] == k) {
-          result++;
-        }
-      }
+      sum += nums[i];
+
+      // add result
+      remainCount = (m.containsKey(sum - k)) ? m.get(sum - k) : 0;
+      // System.out.printf("remain count => %d\n", remainCount);
+      result += remainCount;
+
+
+      // add hash
+      oldHash = m.containsKey(sum) ? m.get(sum) : 0;
+      // System.out.printf("put %d => %d\n", sum, oldHash + 1);
+      m.put(sum, oldHash + 1);
     }
 
     return result;
