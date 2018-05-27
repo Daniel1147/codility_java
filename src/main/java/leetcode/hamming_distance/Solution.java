4ac53985a75a183919664aaf5c0b79714df904d0
Daniel1147 <sxn91401@gmail.com>
2018-05-27 13:16:55 +0800

    [Solution] use Integer.bitCount(). beats 27.13%

--- src/main/java/leetcode/hamming_distance/Solution.java
+++ src/main/java/leetcode/hamming_distance/Solution.java
@@ -1,24 +1,8 @@
 package leetcode.hamming_distance;
 
-import java.util.*;
-
 class Solution {
   public int hammingDistance(int x, int y) {
-    int xor, one, count, result;
 
-    xor = x ^ y;
-    one = 1;
-    count = 0;
-    result = 0;
-
-    while (count < 32) {
-      result += one & xor;
-
-      xor = xor >> 1;
-
-      count++;
-    }
-
-    return result;
+    return Integer.bitCount(x ^ y);
   }
 }
