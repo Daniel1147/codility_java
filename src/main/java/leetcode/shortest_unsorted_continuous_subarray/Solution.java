da7e3613cb2236089741bf42eee14a66fefdec1e
Daniel1147 <sxn91401@gmail.com>
2018-05-28 13:54:21 +0800

    [Solution] use Arrays.sort. beats 38.12%

--- src/main/java/leetcode/shortest_unsorted_continuous_subarray/Solution.java
+++ src/main/java/leetcode/shortest_unsorted_continuous_subarray/Solution.java
@@ -1,8 +1,35 @@
 package leetcode.shortest_unsorted_continuous_subarray;
 
+import java.util.Arrays;
+
 class Solution {
   public int findUnsortedSubarray(int[] nums) {
+    int[] sorted;
+    int start, end;
 
-    return 0;
+    sorted = nums.clone();
+    Arrays.sort(sorted);
+
+    start = nums.length;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != sorted[i]) {
+        start = i;
+
+        break;
+      }
+    }
+    if (start == nums.length)
+      return 0;
+
+    end = -1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      if (nums[i] != sorted[i]) {
+        end = i;
+
+        break;
+      }
+    }
+
+    return end - start + 1;
   }
 }
