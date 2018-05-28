8d5cccca2917a1caaebd5e26ef330709ad667600
Daniel1147 <sxn91401@gmail.com>
2018-05-28 17:14:01 +0800

    [Solution] implement DP. beats 100%

--- src/main/java/leetcode/counting_bits/Solution.java
+++ src/main/java/leetcode/counting_bits/Solution.java
@@ -4,29 +4,12 @@ import java.util.Arrays;
 
 class Solution {
   public int[] countBits(int num) {
-    int count;
-    int[] bits, result;
+    int[] result;
 
-    bits = new int[32];
     result = new int[num + 1];
 
-    count = 0;
-    result[0] = count;
-
     for (int i = 1; i < num + 1; i++) {
-      for (int j = 31; j >= 0; j--) {
-        if (bits[j] == 0) {
-          bits[j] = 1;
-          count++;
-
-          break;
-        }
-
-        bits[j] = 0;
-        count --;
-      }
-
-      result[i] = count;
+      result[i] = result[i / 2] + (i & 1);
     }
 
     return result;
