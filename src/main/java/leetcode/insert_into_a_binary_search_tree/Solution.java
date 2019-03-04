72f0dbe76b56d33d16f42161dd416c3ed594ad74
Daniel1147 <sxn91401@gmail.com>
2019-03-04 21:01:59 +0800

    [Solution] beats 100%

---
+++ src/main/java/leetcode/insert_into_a_binary_search_tree/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.insert_into_a_binary_search_tree;
+
+import java.util.*;
+import leetcode.util.TreeNode;
+
+class Solution {
+  public TreeNode insertIntoBST(TreeNode root, int val) {
+    if (root == null)
+      return new TreeNode(val);
+
+    if (root.val < val) {
+      root.right = insertIntoBST(root.right, val);
+      return root;
+    }
+
+    root.left = insertIntoBST(root.left, val);
+    return root;
+  }
+}
