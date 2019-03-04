4ce818ebb7d00c001e81f565859bc353912a66a5
Daniel1147 <sxn91401@gmail.com>
2019-03-04 16:49:38 +0800

    update test

--- src/test/java/leetcode/sort_array_by_parity/TestSolution.java
+++ src/test/java/leetcode/sort_array_by_parity/TestSolution.java
@@ -1,7 +1,7 @@
 package leetcode.sort_array_by_parity;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 import java.util.Arrays;
 import org.junit.Before;
@@ -9,7 +9,7 @@ import org.junit.Test;
 import com.google.gson.Gson;
 
 public class TestSolution {
-  private int[] expected, actual;
+  private int[] actual;
   private int[] A;
   private Solution s;
 
@@ -26,15 +26,42 @@ public class TestSolution {
   @Test
   public void testLeetcode1() {
     A = new int[] {3, 1, 2, 4};
-    expected = new int[] {2, 4, 1, 3};
 
     myTest();
   }
 
   private void myTest() {
+    int evenCount;
     actual = s.sortArrayByParity(A);
 
-    assertArrayEquals(getFailMsg(), expected, actual);
+    if (A.length != actual.length) {
+      assertTrue(getFailMsg(), false);
+      return;
+    }
+
+    evenCount = 0;
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] % 2 == 0)
+        evenCount++;
+    }
+
+    for (int i = 0; i < evenCount; i++) {
+      if (actual[i] % 2 == 1) {
+        assertTrue(getFailMsg(), false);
+        return;
+      }
+    }
+
+    for (int i = A.length - 1; i >= evenCount; i--) {
+      if (actual[i] % 2 == 0) {
+        assertTrue(getFailMsg(), false);
+        return;
+      }
+    }
+
+    Arrays.sort(A);
+    Arrays.sort(actual);
+    assertArrayEquals(getFailMsg(), A, actual);
   }
 
   private String getFailMsg() {
