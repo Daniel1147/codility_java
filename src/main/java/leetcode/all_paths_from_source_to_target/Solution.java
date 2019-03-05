2a428a06c3b04ad497829b9ef01a72562549114e
Daniel1147 <sxn91401@gmail.com>
2019-03-05 18:58:53 +0800

    [Solution] implement List by ArrayList. beats 44.6%

--- src/main/java/leetcode/all_paths_from_source_to_target/Solution.java
+++ src/main/java/leetcode/all_paths_from_source_to_target/Solution.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 class Solution {
   public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-    List<List<Integer>> result = new LinkedList<List<Integer>>();
-    Queue<LinkedList<Integer>> walkerQueue = new LinkedList<LinkedList<Integer>>();
-    LinkedList<Integer> walker, newList;
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    Queue<ArrayList<Integer>> walkerQueue = new LinkedList<ArrayList<Integer>>();
+    ArrayList<Integer> walker, newList;
     int position, target, nextNode;
 
     target = graph.length - 1;
 
-    LinkedList<Integer> firstWalker = new LinkedList<Integer>();
+    ArrayList<Integer> firstWalker = new ArrayList<Integer>();
     firstWalker.add(0);
     walkerQueue.add(firstWalker);
 
     while (!walkerQueue.isEmpty()) {
       walker = walkerQueue.poll();
 
-      position = walker.getLast();
+      position = walker.get(walker.size() - 1);
       for (int i = 0; i < graph[position].length; i++) {
         nextNode = graph[position][i];
-        newList = new LinkedList<Integer> (walker);
+        newList = new ArrayList<Integer> (walker);
         newList.add(nextNode);
         if (nextNode == target) {
           result.add(newList);
