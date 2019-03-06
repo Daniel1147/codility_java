05664ae9cbb577e9e999a226e4269bf967cd50fd
Daniel1147 <sxn91401@gmail.com>
2019-03-06 23:26:07 +0800

    [Solution] use recursive method but build path top-down. beats 80.67%

--- src/main/java/leetcode/all_paths_from_source_to_target/Solution.java
+++ src/main/java/leetcode/all_paths_from_source_to_target/Solution.java
@@ -3,34 +3,39 @@ package leetcode.all_paths_from_source_to_target;
 import java.util.*;
 
 class Solution {
+  private List<List<Integer>> result;
+  private int[][] graph;
+  private int target;
+  private int n;
+
   public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-    List<List<Integer>> result = new ArrayList<List<Integer>>();
-    Queue<ArrayList<Integer>> walkerQueue = new LinkedList<ArrayList<Integer>>();
-    ArrayList<Integer> walker, newList;
-    int position, target, nextNode;
+    List<Integer> path;
 
-    target = graph.length - 1;
-
-    ArrayList<Integer> firstWalker = new ArrayList<Integer>();
-    firstWalker.add(0);
-    walkerQueue.add(firstWalker);
-
-    while (!walkerQueue.isEmpty()) {
-      walker = walkerQueue.poll();
-
-      position = walker.get(walker.size() - 1);
-      for (int i = 0; i < graph[position].length; i++) {
-        nextNode = graph[position][i];
-        newList = new ArrayList<Integer> (walker);
-        newList.add(nextNode);
-        if (nextNode == target) {
-          result.add(newList);
-        } else {
-          walkerQueue.add(newList);
-        }
-      }
-    }
+    result = new ArrayList<List<Integer>> ();
+    this.graph = graph;
+    n = graph.length;
+    target = n - 1;
+    path = new ArrayList<Integer> ();
+    path.add(0);
+    helper(path, 0);
 
     return result;
+  }
+
+  public void helper(List<Integer> path, int current) {
+    int next;
+    List<Integer> newPath;
+    if (current == target) {
+      result.add(path);
+    }
+
+    for (int i = 0; i < graph[current].length; i++) {
+      next = graph[current][i];
+      newPath = new ArrayList<Integer> (path);
+      newPath.add(next);
+      helper(newPath, next);
+    }
+
+    return;
   }
 }
