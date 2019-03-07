296744a7b2111e6edd43c43febfedcba206b7419
Daniel1147 <sxn91401@gmail.com>
2019-03-07 17:28:07 +0800

    [Solution] use global cache. beats 100%

--- src/main/java/leetcode/all_possible_full_binary_trees/Solution.java
+++ src/main/java/leetcode/all_possible_full_binary_trees/Solution.java
@@ -4,45 +4,50 @@ import java.util.*;
 import leetcode.util.TreeNode;
 
 class Solution {
-  private List<List<TreeNode>> cache;
+  private List<List<TreeNode>> buildCache;
+  private Map<Integer, List<TreeNode>> cache = new HashMap<Integer, List<TreeNode>> ();
   private int MAX_N = 20;
 
   public List<TreeNode> allPossibleFBT(int N) {
     List<TreeNode> listZero, listOne, leftPFBT, rightPFBT, l;
     TreeNode newRoot;
 
-    cache = new ArrayList<List<TreeNode>> ();
+    if (cache.containsKey(N))
+      return cache.get(N);
 
+    buildCache = new ArrayList<List<TreeNode>> ();
     listZero = new ArrayList<TreeNode> ();
-    cache.add(listZero);
+    buildCache.add(listZero);
 
     if (N % 2 == 0)
       return listZero;
 
-    listOne = new ArrayList<TreeNode> ();
-    newRoot = new TreeNode(0);
-    listOne.add(newRoot);
-    cache.add(listOne);
+    if (N == 1) {
+      listOne = new ArrayList<TreeNode> ();
+      newRoot = new TreeNode(0);
+      listOne.add(newRoot);
+      cache.put(1, listOne);
+      return listOne;
+    }
 
-    for (int i = 2; i <= N; i++) {
-      l = new ArrayList<TreeNode> ();
-      cache.add(l);
-      for (int j = 1; j <= i - 2; j+= 2) {
-        leftPFBT = cache.get(j);
-        rightPFBT = cache.get(i - 1 - j);
+    l = new ArrayList<TreeNode> ();
+    for (int j = 1; j <= N - 2; j += 2) {
+      leftPFBT = allPossibleFBT(j);
+      rightPFBT = allPossibleFBT(N - 1 - j);
 
-        for (TreeNode leftTree : leftPFBT) {
-          for (TreeNode rightTree : rightPFBT) {
-            newRoot = new TreeNode(0);
-            newRoot.left = leftTree;
-            newRoot.right = rightTree;
+      for (TreeNode leftTree : leftPFBT) {
+        for (TreeNode rightTree : rightPFBT) {
+          newRoot = new TreeNode(0);
+          newRoot.left = leftTree;
+          newRoot.right = rightTree;
 
-            l.add(newRoot);
-          }
+          l.add(newRoot);
         }
       }
     }
 
-    return cache.get(N);
+    cache.put(N, l);
+
+    return l;
   }
 }
