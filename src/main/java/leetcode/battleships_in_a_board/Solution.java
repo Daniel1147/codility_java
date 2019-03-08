0b7f76d087ca182abb56c11243ca5ff087b0a6e1
Daniel1147 <sxn91401@gmail.com>
2019-03-09 02:11:08 +0800

    [Solution] use array to keep record of column ships. beats 95.99%

--- src/main/java/leetcode/battleships_in_a_board/Solution.java
+++ src/main/java/leetcode/battleships_in_a_board/Solution.java
@@ -3,11 +3,21 @@ package leetcode.battleships_in_a_board;
 import java.util.*;
 
 class Solution {
+  private int DEFAULT_RECORD_LEN = 100;
+
   public int countBattleships(char[][] board) {
-    Set<Integer> record = new HashSet<Integer>();
+    int[] record;
     int ans;
     boolean rowShip;
     char c;
+
+    if (board.length == 0 || board[0].length == 0)
+      return 0;
+
+    if (DEFAULT_RECORD_LEN < board[0].length)
+      record = new int[board[0].length];
+    else
+      record = new int[DEFAULT_RECORD_LEN];
 
     ans = 0;
     for (int i = 0; i < board.length; i++) {
@@ -18,16 +28,16 @@ class Solution {
         if (rowShip) {
           if (c != 'X') {
             rowShip = false;
-            if (record.contains(j))
-              record.remove(j);
+            if (record[j] == 1)
+              record[j] = 0;
           }
 
           continue;
         }
 
-        if (record.contains(j)) {
+        if (record[j] == 1) {
           if (c != 'X')
-            record.remove(j);
+            record[j] = 0;
 
           continue;
         }
@@ -39,7 +49,7 @@ class Solution {
         if (j + 1 < board[i].length && board[i][j + 1] == 'X')
           rowShip = true;
         else {
-          record.add(j);
+          record[j] = 1;
         }
       }
     }
