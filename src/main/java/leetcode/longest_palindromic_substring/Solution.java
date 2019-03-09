6d3c6084604e8772665dbd512efbb37342effda6
Daniel1147 <sxn91401@gmail.com>
2019-03-10 02:13:43 +0800

    [Solution] record not palindrom length but whether it's a palindrom. beats 54.26%

--- src/main/java/leetcode/longest_palindromic_substring/Solution.java
+++ src/main/java/leetcode/longest_palindromic_substring/Solution.java
@@ -5,38 +5,38 @@ import com.google.gson.Gson;
 
 class Solution {
   public String longestPalindrome(String s) {
-    int n, max, len, maxStart, maxEnd;
-    int[][] LPS;
+    int n, max, maxStart, maxEnd;
+    boolean[][] LPS;
     char[] charS;
 
     if (s.length() == 0)
       return "";
 
     n = s.length();
-    LPS = new int[n + 1][n];
+    LPS = new boolean[n + 1][n];
     charS = s.toCharArray();
 
     max = 1;
     maxStart = 0;
     maxEnd = 1;
     for (int i = 0; i < n; i++) {
-      LPS[1][i] = 1;
+      LPS[0][i] = true;
+      LPS[1][i] = true;
     }
 
     for (int i = 2; i <= n; i++) {
       if (i > max + 2)
         break;
       for (int j = 0; j + i - 1 < n; j++) {
-        if (LPS[i - 2][j + 1] != -1 && charS[j] == charS[j + i - 1]) {
-          len = LPS[i - 2][j + 1] + 2;
-          LPS[i][j] = len;
-          if (len > max) {
-            max = len;
+        if (LPS[i - 2][j + 1] && charS[j] == charS[j + i - 1]) {
+          LPS[i][j] = true;
+          if (i > max) {
+            max = i;
             maxStart = j;
             maxEnd = j + i;
           }
         } else {
-          LPS[i][j] = -1;
+          LPS[i][j] = false;
         }
       }
     }
