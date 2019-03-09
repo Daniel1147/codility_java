1ee9cb889c531f0a946bfbfc2f53384cfa96f985
Daniel1147 <sxn91401@gmail.com>
2019-03-10 03:36:43 +0800

    add test

---
+++ src/test/java/leetcode/palindromic_substrings/TestSolution.java
@@ -0,0 +1,42 @@
+package leetcode.palindromic_substrings;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+import com.google.gson.Gson;
+
+public class TestSolution {
+  private int expected, actual;
+  private String s;
+  private Solution solution;
+  private Gson g = new Gson();
+
+  @Before
+  public void setUp() {
+    solution = new Solution();
+  }
+
+  @Test
+  public void testCompile() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void testLeetcode1() {
+    s = "abc";
+    expected = 3;
+
+    myTest();
+  }
+
+  private void myTest() {
+    actual = solution.countSubstrings(s);
+    assertEquals(getFailMsg(), expected, actual);
+  }
+
+  private String getFailMsg() {
+    return s;
+  }
+}
