3f599d470e989e582a32a98c7a212818f7aef8c6
Daniel1147 <sxn91401@gmail.com>
2019-03-12 21:00:57 +0800

    [Solution][leetcode.longest_palindromic_substring.Solution] solve with Manacher's algorithm. beats 99.47%

--- src/main/java/leetcode/longest_palindromic_substring/Solution.java
+++ src/main/java/leetcode/longest_palindromic_substring/Solution.java
@@ -4,47 +4,72 @@ import java.util.*;
 
 class Solution {
   public String longestPalindrome(String s) {
+    int[] lps;
+    int max, iMax, current, len, center, iMirror, n, iLeft, iRight, centerRight, start, end;
     char[] charS;
-    int n, len, max, start, end;
-
-    charS = s.toCharArray();
-    n = charS.length;
 
     if (s.length() == 0)
       return "";
 
-    start = 0;
-    end = 0;
+    n = s.length() * 2 + 1;
+    charS = s.toCharArray();
+    lps = new int[n];
 
+    lps[0] = 0;
+    center = 0;
+    centerRight = 0;
     max = 0;
-    for (int i = 0; i <= 2 * n - 2; i++) {
-      if (i % 2 == 0)
-        len = middlePalindromeLen(charS, i / 2, i / 2);
-      else
-        len = middlePalindromeLen(charS, i / 2, i / 2 + 1);
+    iMax = 0;
 
-      if (max < len) {
-        max = len;
-        if (i % 2 == 0) {
-          start = i / 2 - (len - 1) / 2;
-          end = i / 2 + (len - 1) / 2;
-        } else {
-          start = i / 2 - len / 2 + 1;
-          end = i / 2 + 1 + len / 2 - 1;
+    for (current = 1; current < n; current++) {
+      if (centerRight < current) {
+        // count
+        for (len = 1; current - len >= 0 && current + len < n; len++) {
+          iLeft = ((current - len) - 1) / 2;
+          iRight = ((current + len) - 1) / 2;
+
+          if ((current - len) % 2 == 0)
+            continue;
+
+          if (charS[iLeft] != charS[iRight])
+            break;
         }
+
+        len--;
+      } else {
+        iMirror = 2 * center - current;
+        if (lps[iMirror] + current < centerRight) {
+          len = lps[iMirror];
+        } else {
+          for (len = centerRight - current + 1; current - len >= 0 && current + len < n; len++) {
+            iLeft = ((current - len) - 1) / 2;
+            iRight = ((current + len) - 1) / 2;
+
+            if ((current - len) % 2 == 0)
+              continue;
+
+            if (charS[iLeft] != charS[iRight])
+              break;
+          }
+
+          len--;
+        }
+      }
+      lps[current] = len;
+      if (len > max) {
+        max = len;
+        iMax = current;
+      }
+
+      if (current + len > centerRight) {
+        center = current;
+        centerRight = current + len;
       }
     }
 
+    start = ((iMax - max + 1) - 1) / 2;
+    end = ((iMax + max - 1) - 1) / 2;
 
     return s.substring(start, end + 1);
-  }
-
-  private int middlePalindromeLen(char[] charS, int l, int r) {
-    while (l >= 0 && r < charS.length && charS[l] == charS[r]) {
-      l--;
-      r++;
-    }
-
-    return r - 1 - l - 1 + 1;
   }
 }
