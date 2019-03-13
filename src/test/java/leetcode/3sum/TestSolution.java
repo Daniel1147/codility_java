9a3423bf0f91b82ebe34ae4bad84ee4941a7119f
Daniel1147 <sxn91401@gmail.com>
2019-03-13 17:27:04 +0800

    add tests

---
+++ src/test/java/leetcode/3sum/TestSolution.java
@@ -0,0 +1,55 @@
+package leetcode._3sum;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+import com.google.gson.Gson;
+
+public class TestSolution {
+  private List<List<Integer>> expected, actual;
+  private int[] nums;
+  private Solution s;
+  private Gson g = new Gson();
+
+  @Before
+  public void setUp() {
+    s = new Solution();
+  }
+
+  @Test
+  public void testCompile() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void testLeetcode1() {
+    nums = new int[] {-1, 0, 1, 2, -1, -4};
+    expected = new ArrayList<List<Integer>>();
+    expected.add(Arrays.asList(-1, 0, 1));
+    expected.add(Arrays.asList(-1, -1, 2));
+
+    myTest();
+  }
+
+  @Test
+  public void testLeetcode2() {
+    nums = new int[] {0, 0, 0};
+    expected = new ArrayList<List<Integer>>();
+    expected.add(Arrays.asList(0, 0, 0));
+
+    myTest();
+  }
+
+  private void myTest() {
+    actual = s.threeSum(nums);
+
+    assertThat(getFailMsg(), expected, containsInAnyOrder(actual.toArray()));
+  }
+
+  private String getFailMsg() {
+    return g.toJson(nums);
+  }
+}
