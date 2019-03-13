239cc2306470c164061d87c3d5234f110f1f05d7
Daniel1147 <sxn91401@gmail.com>
2019-03-13 23:42:55 +0800

    [Solution] two pointers. beats 90.05%

--- src/main/java/leetcode/3sum/Solution.java
+++ src/main/java/leetcode/3sum/Solution.java
@@ -5,7 +5,8 @@ import java.util.*;
 class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> result;
-    int i1, i2, i3, sum, oldI1, oldI2, oldI3, layer1I3;
+    List<Integer> list;
+    int i1, i2, i3, sum;
 
     result = new ArrayList<List<Integer>>();
 
@@ -15,62 +16,38 @@ class Solution {
     Arrays.sort(nums);
 
     i1 = 0;
-    layer1I3 = nums.length - 1;
     while (i1 < nums.length - 2) {
       i2 = i1 + 1;
-      i3 = layer1I3;
+      i3 = nums.length - 1;
+
       while (i2 < i3) {
-        sum = -1 * (nums[i1] + nums[i2]);
+        sum = nums[i1] + nums[i2] + nums[i3];
+        if (sum == 0) {
+          list = new ArrayList<Integer>();
+          list.add(nums[i1]);
+          list.add(nums[i2]);
+          list.add(nums[i3]);
+          result.add(list);
 
-        i3 = bSearch(nums, i2 + 1, i3, sum);
-        if (sum == nums[i3])
-          result.add(build(nums[i1], nums[i2], nums[i3]));
-
-        if (i2 == i1 + 1)
-          layer1I3 = i3;
-
-        oldI2 = nums[i2];
-        i2++;
-        while (i2 < nums.length - 1 && nums[i2] == oldI2)
+          while (i2 < i3 && nums[i2 + 1] == nums[i2])
+            i2++;
           i2++;
+
+          while (i3 > i2 && nums[i3 - 1] == nums[i3])
+            i3--;
+          i3--;
+        } else if (sum < 0)
+          i2++;
+        else
+          i3--;
       }
 
-      oldI1 = nums[i1];
-      i1++;
-      while (i1 < nums.length - 2 && nums[i1] == oldI1)
+      // to next i1
+      while (i1 + 1 < nums.length && nums[i1 + 1] == nums[i1])
         i1++;
+      i1++;
     }
 
     return result;
-  }
-
-  private List<Integer> build(int a, int b, int c) {
-    List<Integer> result;
-
-    result = new ArrayList<Integer>();
-    result.add(a);
-    result.add(b);
-    result.add(c);
-
-    return result;
-  }
-
-  private int bSearch(int[] nums, int start, int end, int ceiling) {
-    int middle;
-    if (start >= end)
-      return start;
-
-    middle = ((start + end) / 2) + 1;
-    if (nums[middle] == ceiling) {
-      while (middle - 1 >= start && nums[middle - 1] == ceiling)
-        middle--;
-      return middle;
-    }
-
-    if (nums[middle] > ceiling) {
-      return bSearch(nums, start, middle - 1, ceiling);
-    }
-
-    return bSearch(nums, middle, end, ceiling);
   }
 }
