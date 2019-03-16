4642139d3bf41d4b89220e1b3ced6dc964b400c2
Daniel1147 <sxn91401@gmail.com>
2019-03-16 11:46:07 +0800

    [Solution] use slow-fast pointer method. beats 100%. beats 99.59%(memory)

--- src/main/java/leetcode/find_the_duplicate_number/Solution.java
+++ src/main/java/leetcode/find_the_duplicate_number/Solution.java
@@ -5,18 +5,32 @@ import java.util.*;
 class Solution {
   public int findDuplicate(int[] nums) {
     boolean[] a;
-    int n;
+    int fast, slow, slow2, before;
+    int cnt;
 
-    a = new boolean[nums.length + 1];
+    if (nums.length == 2)
+      return nums[0];
 
-    for (int i = 0; i < nums.length; i++) {
-      n = nums[i];
-      if (a[n])
-        return n;
+    fast = 0;
+    slow = 0;
+    before = 0;
+    cnt = 0;
 
-      a[n] = true;
+    do {
+      before = slow;
+      slow = nums[slow];
+      fast = nums[fast];
+      fast = nums[fast];
+    } while (fast != slow);
+
+    slow2 = 0;
+
+    while (slow2 != slow) {
+      before = slow;
+      slow = nums[slow];
+      slow2 = nums[slow2];
     }
 
-    return -1;
+    return nums[before];
   }
 }
